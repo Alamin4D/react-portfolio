1afd3ee4f95de399c9efb355cc3bfcaa98a112e4
Alamin4D <alaminahmed79000@gmail.com>
2024-07-03 13:55:23 +0600

    added the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
     return (
         <div className='hero'>
-            
+           <img src={profile_img} alt="" />
+           <h1><span>I'm Alamin Hossain,</span> frontend Developer based in BD.</h1>
+           <p>I am a froentend Developer, a passionate web developer and front-end designer with expertise in creating interactive and user-friendly web experiences.</p>
+           <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My resume</div>
+           </div>
         </div>
     );
 };
